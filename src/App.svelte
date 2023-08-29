76a50822774f854ea20266a74f1eec688debbbbb
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-08-29 22:19:10 +0200

    Styling ++

--- src/App.svelte
+++ src/App.svelte
@@ -15,16 +15,17 @@ const parties = [
     { id: "Frp", color: "#22245d", name: "Fremskrittspartiet"},
     { id: "A", color: "#cccccc", name: "Andre"},
 ]
-const listLength = 10
 let data = {}
 let config = {
     party: "",
-    view: ""
+    view: "best",
+    length: 10
 }
 let partyStack = []
 let output = []
 let working = false
 let mounted = false
+let max
 
 function mount(d) {
     data = d
@@ -83,11 +84,17 @@ function update(config) {
         .filter(a => a.siste >= 50)
         .sort((a,b) => b.siste - a.siste)
     }
+    else if (config.view == "best") {
+        output = output
+        .sort((a,b) => b.siste - a.siste)
+    }
     // Add meta
     output.map(a => {
         a.meta = data.data.find(b => b.id == a.id)
         return a
     })
+    max = Math.max(...output.map(e => e.siste), ...output.map(e => e.valg))
+
     output = output
     console.log("Output:",output)
 }
@@ -106,7 +113,6 @@ $: if(mounted) update(config)
 
 // Helpers
 function getColor(id) {
-    console.log(id)
     return parties.find(p => p.id == id).color
 }
 
@@ -115,6 +121,7 @@ function getColor(id) {
 <div class=nav>
     <select class:active={config.view} name="view" bind:value={config.view}>
         <option value="">Velg målemetode</option>
+        <option value="best">Størst oppslutning for ...</option>
         <option value="mostProgress">Mest framgang for ...</option>
         <option value="mostRegress">Mest tilbakegang for ...</option>
         <option value="majority">Flertall for ...</option>
@@ -130,24 +137,28 @@ function getColor(id) {
 {#if output.length > 0}
 <div class="list">
     {#each output as item, i}
-        {#if i < listLength}
-        <div>
-            <div class=municipality>{item.kommune}</div>
-            <div class=content>
-                <div class=name>{item.parti}</div>
-                <div class=bars style="--color: {getColor(item.parti)};">
-                    <div class=latest style="width:{item.siste}%">{item.siste.toLocaleString("nb-NO", {maximumFractionDigits: 1})}%</div>
-                    <div class=v2019 style="width:{item.valg}%">{item.valg.toLocaleString("nb-NO", {maximumFractionDigits: 1})}%</div>
-                </div>
-                <div class=diff>{item.endring > 0 ? "+" + item.endring.toLocaleString("nb-NO", {maximumFractionDigits: 1}) : item.endring.toLocaleString("nb-NO", {maximumFractionDigits: 1})}</div>
+    {#if i < config.length}
+    <div>
+        <div class=municipality>{item.kommune}</div>
+        <div class=content>
+            <div class=name>{item.parti}</div>
+            <div class=bars style="--color: {getColor(item.parti)};">
+                <div style="width:{item.siste / max * 98}%"></div>
+                <div style="width:{item.valg / max * 98}%"></div>
             </div>
+            <div class=latest>{item.siste.toLocaleString("nb-NO", {maximumFractionDigits: 1})}%</div>
+            <div class=valg-2019>{item.valg.toLocaleString("nb-NO", {maximumFractionDigits: 1})}%</div>
+            <div class=diff class:negative={item.endring < 0}>{item.endring > 0 ? "+" : ""}{item.endring.toLocaleString("nb-NO", {maximumFractionDigits: 1})}</div>
         </div>
-        {/if}
+    </div>
+    {/if}
     {/each}
 </div>
-<div>Basert på målinger for {data.data.length} av 357 kommuner. Oppdatert {data.time}</div>
+<div class=credits>Basert på lokale målinger for {data.data.length} av 357 kommuner. Hentet {new Date(data.time).toLocaleString("nb-NO")} fra <a href="https://pollofpolls.no">pollofpolls.no</a>.</div>
 {:else if working}
+<div class=notice>
 Arbeider...
+</div>
 {:else if !config.view && !config.party}
 <div class=notice>
     Velg ovenfor ☝️
@@ -160,6 +171,12 @@ Arbeider...
 {/if}
 
 <style>
+select {
+    padding: 5px 10px;
+    border-radius: 99px;
+    font-size: 18px;
+    font-weight: 500;
+}
 .notice {
     margin-block: 10px;
 }
@@ -167,34 +184,56 @@ Arbeider...
     display: flex;
     flex-direction: column;
     gap: 7.5px;
-    margin-block: 10px;
+    margin-block: 20px;
 }
 .municipality {
     font-weight: bold;
 }
 .content {
     display: flex;   
-    gap: 7.5px;
+    gap: 10px;
 }
 .name {
     width: 30px;
 }
 .bars {
     position: relative;
-    height: 20px;
+    height: 24px;
     flex: 1;
 }
 .bars > div {
     position: absolute;
     left: 0;
-    height: 12px;
+    height: 60%;
     background: var(--color);
 }
-.bars .latest {
+.bars > div:first-child {
     top: 0;
 }
-.bars .v2019 {
+.bars > div:last-child {
     bottom: 0;
-    opacity: .5
+    opacity: .45
+}
+.latest,
+.valg-2019,
+.diff {
+    width: 45px;
+    text-align: right;
+}
+.latest {
+    font-weight: bold;
+}
+.valg-2019 {
+    font-weight: 200;
+}
+.diff {
+    font-weight:bold;
+    color: green;
+}
+.diff.negative {
+    color: red;
+}
+.credits {
+    font-size: .9em;
 }
 </style>
