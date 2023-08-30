d5536c0fb5a7d21f716810d17fb7303a63d191ed
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-08-30 14:13:24 +0200

    Open Sans font-face manual

--- src/App.svelte
+++ src/App.svelte
@@ -141,13 +141,6 @@ function reformatString(string) {
 
 </script>
 
-
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@200;400;500;700&display=swap" rel="stylesheet">
-</svelte:head>
-
 <div class=nav>
     <select class:active={config.view} name="view" bind:value={config.view}>
         <option value="">Velg liste</option>
